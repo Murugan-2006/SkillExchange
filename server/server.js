cf8ff717631cd6588dda94ace41c108496efa6d9
Murugan-2006 <murugan292006m@gmail.com>
2026-04-01 17:38:39 +0530

    Simplify CORS configuration to allow all origins

--- server/server.js
+++ server/server.js
@@ -50,18 +50,9 @@ const allowedOrigins = [
   process.env.CORS_ORIGIN
 ].filter(Boolean);
 
+
 app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (mobile apps, curl, etc.)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: '*',
 }));
 
 // Serve uploads directory as static files
